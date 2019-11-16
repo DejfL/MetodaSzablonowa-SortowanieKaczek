b7edaa8507ff796658d7b2e04f9deb20fd63ef38
Dawid Szymczak <dawidszymczak92@gmail.com>
2019-11-16 13:52:17 +0100

    j

---
+++ MetodaSzablonowaSortowanieKaczek/MetodaSzablonowaSortowanieKaczek/Model/Kaczka.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodaSzablonowaSortowanieKaczek.Model
+{
+    class Kaczka : IComparable
+    {
+        string nazwa;
+        int waga;
+
+        public Kaczka(string nazwa, int waga)
+        {
+            this.nazwa = nazwa;
+            this.waga = waga;
+        }
+
+        public override String ToString()
+        {
+            return nazwa + " wazy " + waga;
+        }
+        public int CompareTo(object obj)
+        {
+            Kaczka kaczka = (Kaczka)obj;
+
+            if (this.waga < kaczka.waga)
+                return -1;
+            else if (this.waga == kaczka.waga)
+                return 0;
+            else
+                return 1;
+        }
+    }
+}
---
+++ MetodaSzablonowaSortowanieKaczek/MetodaSzablonowaSortowanieKaczek/Program.cs
@@ -0,0 +1,39 @@
+﻿using MetodaSzablonowaSortowanieKaczek.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodaSzablonowaSortowanieKaczek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kaczka[] kaczki =
+            {
+                new Kaczka("Kaczor dafy", 32),
+                new Kaczka("Kaczor dewy", 3),
+                new Kaczka("Kaczor donald", 5),
+                new Kaczka("Kaczor howart", 1)
+            };
+
+            Console.WriteLine("Przed sortowaniem");
+            WyswietlKaczki(kaczki);
+
+            Array.Sort(kaczki);
+            Console.WriteLine("Po sortowaniu");
+            WyswietlKaczki(kaczki);
+            Console.ReadKey();
+        }
+
+        static void WyswietlKaczki(Kaczka[] kaczki)
+        {
+            for (int i = 0; i < kaczki.Length; i++)
+            {
+                Console.WriteLine(kaczki[i]);
+            }
+        }
+    }
+}
